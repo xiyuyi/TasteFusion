69c4debd1d2a97e223f4b19324de3cd8d192e13e
xiyuyi <xiyu.yi@gmail.com>
2023-10-17 23:44:22 -0700

    added search button, and adjusted button locations and styles.

---
+++ utils/get_initial_restaurants.py
@@ -0,0 +1,11 @@
+def get_initial_restaurants(center:list = None, search_radius:float = None, mock=False):
+    """
+    This function will query the databse and get the initial ist of restaurants, and
+    return their ids as a list.
+    :param center:
+    :param range:
+    :param mock:
+    :return:
+    """
+    restaurant_ids = []
+    return restaurant_ids
