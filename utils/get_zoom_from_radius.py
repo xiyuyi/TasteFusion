1d9244db4ad4fa9d5cd553f928a3d70a04d551bf
xiyuyi <xiyu.yi@gmail.com>
2023-10-18 11:15:25 -0700

    added the response when a radius input is completed.

---
+++ utils/get_zoom_from_radius.py
@@ -0,0 +1,8 @@
+import random
+
+
+def get_zoom_from_radius(radius=None):
+    """ This function calculate the zoom_start factor based on a radius input value in miles"""
+    # todo - implement this function
+    zoom = random.randint(10, 15)
+    return zoom
